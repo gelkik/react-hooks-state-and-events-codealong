3aadea36a254835315ac636bdadd313f3a361469
George Elkik <gelkik@gmail.com>
2023-01-31 18:52:26 -0500

    final

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useState } from 'react';
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [ toggleOn, setToggleOn ] = useState(false);
+
+  function handleClick() {
+    setToggleOn(toggleOn=>!toggleOn);
+    console.log(toggleOn)
+  }
+  
+  const toggled = toggleOn ? "ON" : "OFF";
+  const color = toggleOn ? 'red' : 'white';
+  console.log(color)
+
+  return <button style = {{background : color}} onClick = {handleClick}>{toggled}</button>;
 }
 
 export default Toggle;
